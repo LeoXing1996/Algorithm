0667b9091974da5197aa4675b66aebe4f81168a7
LeoXing1996 <xingzn1996@hotmail.com>
2018-03-30 12:05:12 +0800

    continue

---
+++ Shiyanlou/BinarySearch/3.cc
@@ -0,0 +1,63 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+class Solution{
+private:
+    int tar;
+    int lowerBound(vector<int>&nums){
+        int lb = -1;
+        int hb = nums.size();
+        int mid = lb + (hb - lb)/2;
+        while(lb<hb){
+            if(nums[mid]>=tar) hb = mid;
+            else lb = mid+1;
+            mid = lb + (hb - lb)/2;
+        }
+        return lb;
+    }
+
+    int higherBound(vector<int>&nums, int low){
+        int lb = low;
+        int hb = nums.size();
+        int mid = lb + (hb - lb)/2;
+        while(lb<hb){
+            if(nums[mid]<=tar) lb = mid;
+            else hb = mid-1;
+            mid = lb + (hb - lb)/2;
+        }
+        return hb;
+    }
+public:
+    vector<int> searchRange(vector<int>& nums, int target){
+        vector<int> res;
+        if(nums.size()==0){
+            res.push_back(-1);
+            res.push_back(-1);
+            return res;
+        }
+        tar = target;
+        int lb = lowerBound(nums);
+        if(nums[lb]!=nums[lb+1]){
+            res.push_back(lb);
+            res.push_back(lb);
+            return res;
+        }
+        int hb = higherBound(nums,lb);
+        res.push_back(lb);
+        res.push_back(hb);
+        return res;
+    }
+};
+
+int main(){
+    int a[6] = {1,2,2,2,3,3};
+    vector<int> nums(a,a+5);
+    int target = 2;
+    Solution sol;
+    vector<int>res = sol.searchRange(nums, target);
+    cout << res[0] << ' ' << res[1] << endl;
+
+    return 0;
+}
