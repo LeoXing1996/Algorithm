67ef95722346bd64c50b93b85cdad8788c0e8eb3
LeoXing1996 <xingzn1996@hotmail.com>
2018-04-02 13:14:32 +0800

    continue

--- Shiyanlou/BinarySearch/9.cc
+++ Shiyanlou/BinarySearch/9.cc
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<limits.h>
 
 using namespace std;
 
@@ -38,17 +39,43 @@ public:
     }
 };
 
-// class Solution2{
-// public:
-//     double findMedianSortedArrays(vector<int>&A, vector<int>&B){
-
-//     }
-// };
+class Solution2{
+private:
+    int findkth(vector<int>&A, int A_start, vector<int>&B, int B_start, int k){
+        //A B 中某一个被完全去除
+        if(A_start>=A.size())
+            return B[B_start+k-1];
+        if(B_start>=B.size())
+            return A[A_start+k-1];
+        //递归终止
+        if(k==1)
+            return A[A_start]>B[B_start] ? B[B_start] : A[A_start];
+        
+        int A_key = A_start + k/2 - 1 <= A.size() - 1? 
+                    A[A_start + k/2 - 1] : INT_MAX;
+        int B_key = B_start + k/2 - 1 <= B.size() - 1? 
+                    B[B_start + k/2 - 1] : INT_MAX;
+        if(A_key>B_key)
+            return findkth(A, A_start, B, B_start + k/2, k-k/2);
+        else    
+            return findkth(A, A_start + k/2, B, B_start, k-k/2);
+    }
+public:
+    double findMedianSortedArrays(vector<int>&A, vector<int>&B){
+        int SIZE = A.size() +  B.size();
+        int K = SIZE / 2;
+        if(SIZE % 2 == 1)
+            return findkth(A, 0, B, 0, K+1);
+        else{
+            return (double(findkth(A, 0, B, 0, K)) + double(findkth(A, 0, B, 0, K+1)))/2;
+        }    
+    }
+};
 
 int main(){
     vector<int>A = {1,2,3,4,5,6};
     vector<int>B = {2,3,4,5};
-    Solution sol;
+    Solution2 sol;
     cout  << sol.findMedianSortedArrays(A, B);
     return 0;
 }
